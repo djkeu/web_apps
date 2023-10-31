53628f11a36e2790ecf862e8494cfa743b29f08c
djkeu <mrkooij@gmail.com>
2023-10-31 11:16:38 +0100

    Create method __str__()

--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -2,4 +2,10 @@ from django.db import models
 
 class Topic(models.Model):
     """A topic the user is learning about."""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        """Return a string represantation of the model."""
+        return self.text
     
