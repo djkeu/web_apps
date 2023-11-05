d629b1d08838516f1ecb95d51843a4c7dad35a59
djkeu <mrkooij@gmail.com>
2023-11-05 12:21:00 +0100

    Add method __str__

--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -5,4 +5,6 @@ class Topic(models.Model):
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
-    
+    def __str__(self) -> str:
+        """Return a string representation of the model."""
+        return self.text
