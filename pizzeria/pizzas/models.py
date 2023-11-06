82170c00aeaadf38a0b9b8a6001739a146c2b6fd
djkeu <mrkooij@gmail.com>
2023-11-06 12:56:22 +0100

    Create class Pizza()

--- pizzeria/pizzas/models.py
+++ pizzeria/pizzas/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Pizza(models.Model):
+    """Model a pizza."""
+    name = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        """Return a string presentation of the pizza."""
+        return self.name
