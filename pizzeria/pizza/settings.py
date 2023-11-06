e1042faee18ca0e0c4bb412947b0a5e7b90473cf
djkeu <mrkooij@gmail.com>
2023-11-06 12:56:54 +0100

    Add reference to admin

--- pizzeria/pizza/settings.py
+++ pizzeria/pizza/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # my apps
+    'pizzas',
+
+    # django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
