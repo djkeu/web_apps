618230a166fb590918a71ff7bec7c366c64e7c88
djkeu <mrkooij@gmail.com>
2023-11-06 15:58:09 +0100

    Add comments to /learning_logs/urls.py

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -6,5 +6,5 @@ from . import views  # import views.py
 app_name = 'learning_logs'
 urlpatterns = [
     # Home page
-    path('', views.index, name='index')
+    path('', views.index, name='index')  # def index() in views.py
 ]
