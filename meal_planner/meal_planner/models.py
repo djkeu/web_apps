9f766b3f92908743d75988e6acbad22c6955b273
djkeu <mrkooij@gmail.com>
2023-11-06 22:13:53 +0100

    Create model Meal()

--- meal_planner/meal_planner/models.py
+++ meal_planner/meal_planner/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Meal(models.Model):
+    """Model a meal."""
+    text = models.CharField(max_length=20)
+
+    def __str__(self) -> str:
+        """Return string representation of a meal."""
+        return self.text
+    
