f056e7aae78e450abc408874bf0e56681205aded
djkeu <mrkooij@gmail.com>
2023-11-06 13:28:22 +0100

    Create Pizza()

--- pizzeria/pizzas/models.py
+++ pizzeria/pizzas/models.py
@@ -7,3 +7,15 @@ class Pizza(models.Model):
     def __str__(self) -> str:
         """Return a string presentation of the pizza."""
         return self.name
+
+class Topping(models.Model):
+    """Model the toppings of a pizza."""
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    topping_name = models.TextField()
+
+    class Meta:
+        verbose_name_plural = 'toppings'
+
+    def __str__(self) -> str:
+        """Return a string represenation of the topping."""
+        return f"{self.topping_name}"
