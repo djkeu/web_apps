805cf88563a197c709a7185a271d9737610553ef
djkeu <mrkooij@gmail.com>
2023-11-06 15:41:57 +0100

    Populate urls.py

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -1,1 +1,10 @@
 """Defines url patterns for learning_logs."""
+
+from django.urls import path
+from . import views  # import views.py
+
+app_name = 'learning_logs'
+urlpatterns = [
+    # Home page
+    path('', views.index, name='index')
+]
