44f64311d053363a60c47508274c7f02ee048417
djkeu <mrkooij@gmail.com>
2023-11-08 00:35:05 +0100

    Create def pizzas()

--- pizzeria/pizzas/views.py
+++ pizzeria/pizzas/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import Pizza
 
 # Create your views here.
 def index(request):
     """Home page for Pizzeria."""
     return render(request, 'pizzas/index.html')
+
+def pizzas(request):
+    pizzas = Pizza.objects
+    context = {'pizzas': pizzas}
+    return render(request, 'pizzas/pizzas.html', context)
