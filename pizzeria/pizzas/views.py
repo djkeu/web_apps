3aa32151071e6b362467da9641a78ababb756e6a
djkeu <mrkooij@gmail.com>
2023-11-08 12:31:06 +0100

    Add single pizza view

--- pizzeria/pizzas/views.py
+++ pizzeria/pizzas/views.py
@@ -7,6 +7,14 @@ def index(request):
     return render(request, 'pizzas/index.html')
 
 def pizzas(request):
+    """All pizza's."""
     pizzas = Pizza.objects.order_by('id')
     context = {'pizzas': pizzas}
     return render(request, 'pizzas/pizzas.html', context)
+
+def pizza(request, topic_id):
+    """Single pizza with toppings."""
+    pizza = Pizza.objects.get(id=topic_id)
+    toppings = pizza.entry_set
+    context = {'pizza': pizza, 'toppings': toppings}
+    return render(request, 'pizzas/pizza.html', context)
