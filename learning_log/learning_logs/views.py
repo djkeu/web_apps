105f425dd979519cb6c09c7a628ea6a2296f76b2
djkeu <mrkooij@gmail.com>
2023-11-09 12:48:48 +0100

    Create new_topic()

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from . models import Topic
+from django.shortcuts import render, redirect
+from .models import Topic
+from .forms import TopicForm
 
 # Create your views here.
 
@@ -19,3 +20,19 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')  # ´-dat..´: reverse order
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """Add new topic."""
+    if request.method != 'POST':
+        # No data submitted, create blank form
+        form = TopicForm()
+    else:
+        # POST data submitted; process data
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+        
+    # Display a blank or invalid form
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
