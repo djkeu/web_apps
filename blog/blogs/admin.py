6579c162aee95e9c74ddfc849cec3b6c14ce06a9
djkeu <mrkooij@gmail.com>
2023-11-10 20:44:31 +0100

    Create and register Blog()

--- blog/blogs/admin.py
+++ blog/blogs/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import BlogPost
+from .models import Blog, BlogPost
 
 # admin.site.register(Blog)
+admin.site.register(Blog)
 admin.site.register(BlogPost)
