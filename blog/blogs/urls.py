77fd3e694a1ff78f0f6546fc4cdb32c60eafc283
djkeu <mrkooij@gmail.com>
2023-11-10 13:01:50 +0100

    Reverse changes

--- blog/blogs/urls.py
+++ blog/blogs/urls.py
@@ -6,6 +6,4 @@ app_name = 'blogs'
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
-    # Blog posts
-    path('blogposts/<int:blogpost_id>/', views.blogpost, name='blogpost'),
 ]
