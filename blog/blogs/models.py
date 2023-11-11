614b5b2b90bbaee121288ecd526902c4e6a1395b
djkeu <mrkooij@gmail.com>
2023-11-11 14:46:28 +0100

    Correct date_addes -> date_added

--- blog/blogs/models.py
+++ blog/blogs/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class BlogPost(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
-    date_addes = models.DateTimeField(auto_now_add=True)
+    date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
         return self.title
