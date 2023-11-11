ee0cef5a851a74e95cefc72157d825da431dc234
djkeu <mrkooij@gmail.com>
2023-11-11 20:34:03 +0100

    Attempt to show all posts on blogposts.html

--- blog/blogs/views.py
+++ blog/blogs/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 
+from .models import BlogPost
 # Create your views here.
 
 def index(request):
     """Home page for blogs."""
     return render(request, 'blogs/index.html')
 
-def blogposts(request):
+def blogpost(request, blogpost_id):
     """Page for all blogposts."""
-    return render(request, 'blogposts/html')
+    blogpost = BlogPost.objects.get(id=blogpost_id)
+    blogposts = blogpost.blogpost_set.order_by('-date_added')
+
+    context = {'blogpost': blogpost, 'blogposts': blogposts}
+    return render(request, 'blogposts/html', context)
+    # FixMe: blogposts not showing
+
