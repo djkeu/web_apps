1f599c0a6c8f500c77b59f81bd81ef1871dfff77
djkeu <mrkooij@gmail.com>
2023-11-17 19:15:37 +0100

    Create view for new post

--- blog/blogs/views.py
+++ blog/blogs/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import BlogPost
+from forms import BlogForm
 
 # Create your views here.
 def index(request):
@@ -13,3 +14,20 @@ def blogposts(request):
     blogposts = blogposts.order_by('-date_added')
     context = {'blogposts': blogposts}
     return render(request, 'blogs/index.html', context)
+
+def new_post(request):
+    """Add a new post."""
+    if request.method != 'POST':
+        # No data submitted; create blank form
+        form = BlogForm()
+
+    else:
+        # POST data submitted;  process data
+        form = BlogForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('blogs:blogposts')
+        
+    # Display blank or invalid form
+    context = [{'form': form}]
+    return render(request, 'blogs/new_topic.html', context)
