2a5a4ef01a886032793188acf758b6dbef88dc92
djkeu <mrkooij@gmail.com>
2023-11-17 17:02:54 +0100

    Add url to single post

--- blog/blogs/urls.py
+++ blog/blogs/urls.py
@@ -8,5 +8,6 @@ app_name = 'blogs'
 urlpatterns = [
     # Home page
     #path('', views.index, name='index'),
-    path('', views.blogposts, name='index')
+    path('', views.blogposts, name='index'),
+    path('/<int:blogpost_id', views.blogpost, name='blogpost'),
 ]
