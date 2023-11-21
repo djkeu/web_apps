ada4f76d609d1398f846715b2a41a92e09573cfa
djkeu <mrkooij@gmail.com>
2023-11-21 09:22:51 +0100

    Import Http404

--- blog/blogs/views.py
+++ blog/blogs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 from .models import BlogPost
 from .forms import BlogForm
